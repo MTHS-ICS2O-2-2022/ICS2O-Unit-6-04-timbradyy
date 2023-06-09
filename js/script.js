eb2b276c4b3fb13262524468d42ddbc4672d47bd
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-06-09 22:09:00 +0000

    bread

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Tim Brady
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+  const radius = params.get("r")
+
+  document.getElementById("radius").innerHTML =
+    "The radius is: " + radius + " cm."
+
+  const volume = (4.0 * Math.PI * radius ** 3) / 3.0
+  document.getElementById("volume-of-sphere").innerHTML =
+    "The volume of the sphere is: " + volume.toFixed(2) + " cm³."
+}
